dd09e8992aee49a137c000444bc6e3a9fc4f7873
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-02 13:21:46 +0400

    Import Header in App

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React from "react";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <header class="header" id="header">
-        <nav class="nav container">
-          <a href="#" class="nav__logo">
-            <img src="assets/img/logo.png" alt="" />
-          </a>
-
-          <div class="nav__menu" id="nav-menu">
-            <ul class="nav__list">
-              <li class="nav__item">
-                <a href="#home" class="nav__link active-link">
-                  Home
-                </a>
-              </li>
-              <li class="nav__item">
-                <a href="#specs" class="nav__link">
-                  Specs
-                </a>
-              </li>
-              <li class="nav__item">
-                <a href="#case" class="nav__link">
-                  Case
-                </a>
-              </li>
-              <li class="nav__item">
-                <a href="#products" class="nav__link">
-                  Products
-                </a>
-              </li>
-            </ul>
-
-            <div class="nav__close" id="nav-close">
-              <i class="ri-close-line"></i>
-            </div>
-          </div>
-
-          <div class="nav__toggle" id="nav-toggle">
-            <i class="ri-function-line"></i>
-          </div>
-        </nav>
-      </header>
+      <Header />
 
       <main class="main">
         <section class="home section" id="home">
