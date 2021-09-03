96375462cf9b312f26f72afdaedd239b18cc455b
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-03 22:24:50 +0400

    Push

--- src/pages/index/IndexPages.js
+++ src/pages/index/IndexPages.js
@@ -5,6 +5,7 @@ import Sponsors from "../../components/sponsors/Sponsors";
 import Home from "../../components/home/Home";
 import Specs from "../../components/specs/Specs";
 import Case from "../../components/case/Case";
+import Notification from "../../components/notification/Notification";
 
 export default function IndexPages() {
   return (
@@ -16,29 +17,7 @@ export default function IndexPages() {
         <Sponsors />
         <Specs />
         <Case />
-
-        <section className="discount section">
-          <div className="discount__container container grid">
-            <div className="discount__animate">
-              <h2 className="discount__title">
-                ჩაეშვი <br /> მუსიკის სამყაროში
-              </h2>
-              <p className="discount__description">
-                იყიდე ახლა 50%-იანი ფასდაკლებით.
-              </p>
-              <a href="#" className="button button--flex">
-                <i className="ri-shopping-bag-line button__icon"></i> ყიდვა
-              </a>
-            </div>
-
-            <img
-              src="assets/img/discount.png"
-              alt=""
-              className="discount__img"
-            />
-          </div>
-        </section>
-
+        <Notification />
         <section className="products section" id="products">
           <h2 className="section__title section__title-gradient products__line">
             აირჩიე სტილი
