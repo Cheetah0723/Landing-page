a04d1c8927e0e36cf9530bdc40cf639c3bee1b7f
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-03 22:51:31 +0400

    Products

--- src/pages/index/IndexPages.js
+++ src/pages/index/IndexPages.js
@@ -6,6 +6,7 @@ import Home from "../../components/home/Home";
 import Specs from "../../components/specs/Specs";
 import Case from "../../components/case/Case";
 import Notification from "../../components/notification/Notification";
+import Products from "../../components/products/Products";
 
 export default function IndexPages() {
   return (
@@ -18,95 +19,7 @@ export default function IndexPages() {
         <Specs />
         <Case />
         <Notification />
-        <section className="products section" id="products">
-          <h2 className="section__title section__title-gradient products__line">
-            აირჩიე სტილი
-          </h2>
-
-          <div className="products__container container grid">
-            <article className="products__card">
-              <div className="products__content">
-                <img
-                  src="assets/img/product1.png"
-                  alt=""
-                  className="products__img"
-                />
-
-                <h3 className="products__title">შავი</h3>
-                <span className="products__price">$249</span>
-
-                <button className="button button--flex products__button">
-                  <i className="ri-shopping-bag-line button__icon"></i>
-                </button>
-              </div>
-            </article>
-            <article className="products__card">
-              <div className="products__content">
-                <img
-                  src="assets/img/product2.png"
-                  alt=""
-                  className="products__img"
-                />
-
-                <h3 className="products__title">წითელი შავი</h3>
-                <span className="products__price">$249</span>
-
-                <button className="button button--flex products__button">
-                  <i className="ri-shopping-bag-line button__icon"></i>
-                </button>
-              </div>
-            </article>
-            <article className="products__card">
-              <div className="products__content">
-                <img
-                  src="assets/img/product3.png"
-                  alt=""
-                  className="products__img"
-                />
-
-                <h3 className="products__title">მუქი ლურჯი</h3>
-                <span className="products__price">$249</span>
-
-                <button className="button button--flex products__button">
-                  <i className="ri-shopping-bag-line button__icon"></i>
-                </button>
-              </div>
-            </article>
-            <article className="products__card">
-              <div className="products__content">
-                <img
-                  src="assets/img/product4.png"
-                  alt=""
-                  className="products__img"
-                />
-
-                <h3 className="products__title">ლურჯი</h3>
-                <span className="products__price">$249</span>
-
-                <button className="button button--flex products__button">
-                  <i className="ri-shopping-bag-line button__icon"></i>
-                </button>
-              </div>
-            </article>
-
-            <article className="products__card">
-              <div className="products__content">
-                <img
-                  src="assets/img/product5.png"
-                  alt=""
-                  className="products__img"
-                />
-
-                <h3 className="products__title">ბინდის ვერცხლისფერი</h3>
-                <span className="products__price">$249</span>
-
-                <button className="button button--flex products__button">
-                  <i className="ri-shopping-bag-line button__icon"></i>
-                </button>
-              </div>
-            </article>
-          </div>
-        </section>
+        <Products />
       </main>
       <Footer />
 
