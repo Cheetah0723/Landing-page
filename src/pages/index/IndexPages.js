7bf9160d5e615e4a12c4260f7075be9d8e8f02c7
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-03 21:54:15 +0400

    Specs Component

--- src/pages/index/IndexPages.js
+++ src/pages/index/IndexPages.js
@@ -3,6 +3,7 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Sponsors from "../../components/sponsors/Sponsors";
 import Home from "../../components/home/Home";
+import Specs from "../../components/specs/Specs";
 
 export default function IndexPages() {
   return (
@@ -12,46 +13,7 @@ export default function IndexPages() {
         <Home />
 
         <Sponsors />
-
-        <section className="specs section grid" id="specs">
-          <h2 className="section__title section__title-gradient">
-            მახასიათებლები
-          </h2>
-
-          <div className="specs__container container grid">
-            <div className="specs__content grid">
-              <div className="specs__data">
-                <i className="ri-bluetooth-line specs__icon"></i>
-                <h3 className="specs__title">კავშირი</h3>
-                <span className="specs__subtitle">Bluetooth v5.2</span>
-              </div>
-
-              <div className="specs__data">
-                <i className="ri-battery-charge-line specs__icon"></i>
-                <h3 className="specs__title">ელემენტი</h3>
-                <span className="specs__subtitle">იმუშავებს 40სთ</span>
-              </div>
-
-              <div className="specs__data">
-                <i className="ri-plug-line specs__icon"></i>
-                <h3 className="specs__title">დატენვა</h3>
-                <span className="specs__subtitle">სწრაფი დატენვა 4.2-AAC</span>
-              </div>
-
-              <div className="specs__data">
-                <i className="ri-mic-line specs__icon"></i>
-                <h3 className="specs__title">მიკროფონი</h3>
-                <span className="specs__subtitle">
-                  აქვს მხარდაჭერა Apple Siri-ის <br /> და Google-ის
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <img src="assets/img/specs.png" alt="" className="specs__img" />
-            </div>
-          </div>
-        </section>
+        <Specs />
 
         <section className="case section grid" id="case">
           <h2 className="section__title section__title-gradient">ქეისი</h2>
