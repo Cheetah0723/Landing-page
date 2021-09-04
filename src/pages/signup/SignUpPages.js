89449e8c9aed9c1a56d28aaf11ee0d3bcdfb046d
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-04 17:36:55 +0400

    signup page

--- src/pages/signup/SignUpPages.js
+++ src/pages/signup/SignUpPages.js
@@ -1,11 +1,91 @@
 import React from 'react'
 
 function SignUpPages() {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div className="signin__head-header signup__head">
+        <div className="signin__head-header-wrapper">საიტზე რეგისტრაცია</div>
+      </div>
+      <form action="">
+        <div className="signup__block-2elements">
+          <div class="form__div">
+            <input
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              class="form__input"
+              placeholder=" "
+            />
+            <label for="" class="form__label">
+              სახელი
+            </label>
+          </div>
+          <div class="form__div">
+            <input
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              class="form__input"
+              placeholder=" "
+            />
+            <label for="" class="form__label">
+              გვარი
+            </label>
+          </div>
         </div>
-    )
+        <div className="signup__block-1elements">
+          <div class="form__div">
+            <input
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              class="form__input"
+              placeholder=" "
+            />
+            <label for="" class="form__label">
+              ელ-ფოსტა
+            </label>
+          </div>
+        </div>
+        <div className="signup__block-2elements">
+          <div class="form__div">
+            <input
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              class="form__input"
+              placeholder=" "
+            />
+            <label for="" class="form__label">
+              პაროლი
+            </label>
+          </div>
+          <div class="form__div">
+            <input
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              class="form__input"
+              placeholder=" "
+            />
+            <label for="" class="form__label">
+              გაიმეორეთ პაროლი
+            </label>
+          </div>
+        </div>
+        <div className="form__buttonContainer signup__button-box">
+          <button className="form__signinButton signup__button">
+            რეგისტრაცია
+          </button>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default SignUpPages
