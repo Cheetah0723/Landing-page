afd810f529e44ff0ed447dd7dca53c12dfcebecf
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-04 21:51:12 +0400

    login && signup

---
+++ server/router/login/LoginRouter.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const UserSchema = require("../../schema/user/user");
+const bcrypt = require("bcrypt");
+
+router.route("/login").post(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    
+    UserSchema.findOne({ email: email }).then((result) => {
+      if (result == null) {
+        res.json({ message: "User does not exist", success: false });
+      } else if (result.password.length > 0) {
+        bcrypt.compare(password, result.password, (err, verified) => {
+          if (verified) {
+            user = result;
+            res.json({
+              user: {
+                firstName: result.firstName,
+                lastName: result.lastName,
+                email: result.email,
+              },
+              success: true,
+            });
+          } else {
+            res.json({ message: "The password is incorrect", success: false });
+          }
+        });
+      }
+    });
+  });
+  
+  module.exports = router;
