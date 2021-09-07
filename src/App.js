cfc27fb14f4c131234a7dc23f18b84a50436a103
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-07 14:52:03 +0400

    prod

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import IndexPages from "./pages/index/IndexPages";
-import axios from "axios";
-import { ApplicationContext } from "./context/application/ApplicationContext";
-import env from "./application/environment/env.json";
-import SignUpPages from "./pages/signup/SignUpPages";
+import React, { useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import IndexPages from './pages/index/IndexPages'
+import axios from 'axios'
+import { ApplicationContext } from './context/application/ApplicationContext'
+import env from './application/environment/env.json'
+import SignUpPages from './pages/signup/SignUpPages'
 function App() {
-  const [application, setApplication] = React.useState([]);
-  const [notification, setNotification] = React.useState([]);
+  const [application, setApplication] = React.useState([])
+  const [notification, setNotification] = React.useState([])
   React.useEffect(() => {
     axios.post(`${env.host}/api/sponsors`).then((res) => {
-      setApplication(res.data.data);
-    });
+      setApplication(res.data.data)
+    })
+    axios.post(`${env.host}/api/products`).then((res) => {
+      console.log('lag')
+    })
     axios.get(`${env.host}/api/notifications/read`).then((res) => {
-      setNotification(res.data.data);
-    });
-  }, []);
+      setNotification(res.data.data)
+    })
+  }, [])
   useEffect(() => {
-    if (localStorage.getItem("lang") == "en") {
-      document.body.classList.add("en");
+    if (localStorage.getItem('lang') == 'en') {
+      document.body.classList.add('en')
     }
-  });
+  })
   return (
     <>
       <ApplicationContext.Provider
@@ -34,7 +37,7 @@ function App() {
         </BrowserRouter>
       </ApplicationContext.Provider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
