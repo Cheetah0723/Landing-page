c09da803648444dc8bc2a47f3d7b1518d0b156b2
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-07 23:15:36 +0400

    Add Middleware

---
+++ server/middlewares/loginMiddlewares.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const env = require("../env.json");
+
+module.exports = async function (req, res, next) {
+  const token = JSON.parse(
+    JSON.stringify(req.headers.authorization.split(" ")[1])
+  );
+  try {
+    if (!token) {
+      res.json({ msg: "ტოკენი არ არსებობს" });
+      next();
+    } else {
+      const decodedData = jwt.verify(token, env.ACCESS_TOKEN);
+      req.email = decodedData.email;
+      req.role = decodedData.role;
+      next();
+    }
+  } catch (err) {
+    res.json({ msg: "თქვენი სესია ამოიწურა", expired: true });
+  }
+};
