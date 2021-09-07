7a7b24539a4b1a0fa69b58298e7bb6729d01a4a8
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-07 14:49:02 +0400

    Update ProductsRouter.js

--- server/router/products/ProductsRouter.js
+++ server/router/products/ProductsRouter.js
@@ -1,31 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const Products = require("../../schema/products/ProductSchema");
 
-
-router.route('/products').get(async (req, res) => {
-    try {
-        const products = await Products.find();
-        res.json({ products });
-    } catch (err) {
-        res.json({ message: err });
-    }
+router.route("/products").get(async (req, res) => {
+  try {
+    const products = await Products.find();
+    res.json({ products });
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+// router.route('/products').post(async (req, res) => {
+//     const Product = new Products({
+//         title: req.body.title,
+//         price: req.body.price,
+//         image: req.body.image
+//     });
 
-router.route('/products').post(async (req, res) => {
-    const Product = new Products({
-        title: req.body.title,
-        price: req.body.price,
-        image: req.body.image
-    });
-
-    try {
-        const savedProduct = await Product.save();
-        res.json({ success: true });
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
-
+//     try {
+//         const savedProduct = await Product();
+//         res.json({ products: savedProduct, success: true });
+//     } catch (err) {
+//         res.json({ message: err });
+//     }
+// });
 
 module.exports = router;
